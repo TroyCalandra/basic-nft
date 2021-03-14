00418760f127cb7ba0a5c78278d574ecdb8fb494
Troy Calandra <troydcalandra@gmail.com>
2021-03-13 22:31:18 -0500

    Add ERC721Enumerable to contract

--- src/contracts/Color.sol
+++ src/contracts/Color.sol
@@ -1,14 +1,13 @@
-pragma solidity 0.8.0;
+pragma solidity 0.8.2;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract Color is ERC721 {
+
+contract Color is ERC721Enumerable {
     string[] public colors;
     mapping(string => bool) _colorExists;
 
-    constructor() ERC721("Color", "COLOR") public {
-
-    }
+    constructor() ERC721("Color", "COLOR") public {}
 
     // E.G. color = #FFFFFF
     function mint(string memory _color) public {
