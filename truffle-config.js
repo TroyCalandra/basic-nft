8228db8ad6caa8afd03301960a40b4543522aeae
Troy Calandra <troydcalandra@gmail.com>
2021-03-15 22:55:21 -0400

    Add Ropsten support

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,4 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 require('babel-register');
 require('babel-polyfill');
 
@@ -8,6 +9,11 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     },
+    ropsten: {
+      provider: () => new HDWalletProvider('', 'https://ropsten.infura.io/v3/5f656cd2dda844be8a102549be7c1f6c'),
+      network_id: 3, // Ropsten's id
+      gas: 8000000, // Ropsten has a lower block limit than mainnet
+    }
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
